79586f1f95d57fb7700cae7d41db2d078d01598c
iLoopBreak <teogamer908@gmail.com>
2024-05-25 19:31:28 +0200

    feat: Added IntentModelRepository for handling search and CRUD

--- src/main/java/me/loopbreak/hermesanalyzer/repository/IntentModelRepository.java
+++ src/main/java/me/loopbreak/hermesanalyzer/repository/IntentModelRepository.java
@@ -1,7 +1,8 @@
 package me.loopbreak.hermesanalyzer.repository;
 
 import me.loopbreak.hermesanalyzer.entity.IntentModelEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
-public interface IntentModelRepository extends CrudRepository<IntentModelEntity, String> {
+public interface IntentModelRepository extends ListCrudRepository<IntentModelEntity, String> {
+    IntentModelEntity findByDisplayNameLikeIgnoreCase(String displayName);
 }
