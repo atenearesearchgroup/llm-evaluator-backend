dd06ba3557e2b6eef3fbbdce79d599a887ff307e
iLoopBreak <teogamer908@gmail.com>
2024-09-05 11:10:16 +0200

    chore: Added maxScore to EvaluationResult

--- src/main/java/me/loopbreak/hermesanalyzer/objects/evaluator/EvaluatorConnectorImpl.java
+++ src/main/java/me/loopbreak/hermesanalyzer/objects/evaluator/EvaluatorConnectorImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class EvaluatorConnectorImpl implements EvaluatorConnector {
 
-    private static final EvaluationResult DEFAULT_RESULT = new EvaluationResult(0.0, new ArrayList<>(), null);
+    private static final EvaluationResult DEFAULT_RESULT = new EvaluationResult(0.0, 0.0, new ArrayList<>(), null);
     private static final String URL = "https://api.huggingface.co";
 
 //    private final RestClient client = getClient();
--- src/main/java/me/loopbreak/hermesanalyzer/objects/evaluator/response/EvaluationResult.java
+++ src/main/java/me/loopbreak/hermesanalyzer/objects/evaluator/response/EvaluationResult.java
@@ -2,9 +2,9 @@ package me.loopbreak.hermesanalyzer.objects.evaluator.response;
 
 import java.util.List;
 
-public record EvaluationResult(double score, List<CategoryError> errors, String diagram) {
+public record EvaluationResult(double score, double maxScore, List<CategoryError> errors, String diagram) {
 
     public EvaluationResult withDiagram(String diagram) {
-        return new EvaluationResult(score, errors, diagram);
+        return new EvaluationResult(score, maxScore, errors, diagram);
     }
 }
